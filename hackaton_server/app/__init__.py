38cf5cd3d8b0d2df7abb6828d34f021ba235648b
boksh <e.bokshitskiy@hh.ru>
2018-10-05 12:49:53 +0300

    frontik app

--- hackaton_server/app/__init__.py
+++ hackaton_server/app/__init__.py
@@ -1,1 +1,7 @@
 # coding=utf-8
+
+from frontik.app import FrontikApplication
+
+class HackApplication(FrontikApplication):
+    def __init__(self, **settings):
+        super(HackApplication, self).__init__(**settings)
