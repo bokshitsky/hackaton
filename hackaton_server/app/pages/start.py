9299f9483cb0b107ffbea018287f97883068b403
boksh <e.bokshitskiy@hh.ru>
2018-10-05 23:34:08 +0300

    temp

--- hackaton_server/app/pages/start.py
+++ hackaton_server/app/pages/start.py
@@ -2,8 +2,10 @@ import frontik.handler
 
 class Page(frontik.handler.PageHandler):
     def get_page(self):
-
+        self.set_template('start.html')
         self.json.put({
             'text': 'Hello, world!'
         })
 
+    def post_page(self):
+        self.redirect('/stage')
